3e7558d6bf9c7aa530aa46b783b22bad0213e5a8
Martin Fournier <martin@mfournier.com>
2019-12-20 13:11:15 -0500

    Move application into src folder

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const config = require('./config.json');
+const config = require('../config.json');
 const data = require('./data');
 
 const bot = new Discord.Client();
